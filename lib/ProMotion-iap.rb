a8dc5b161b3266e8e0a88960ca68577cad6a7992
Jamon Holmgren <jamon@clearsightstudio.com>
2015-04-28 11:50:13 -0700

    Remove abort

--- lib/ProMotion-iap.rb
+++ lib/ProMotion-iap.rb
@@ -4,8 +4,7 @@ unless defined?(Motion::Project::Config)
   raise "ProMotion-iap must be required within a RubyMotion project."
 end
 
-Motion::Project::App.pre_setup do |app|
-  abort "8"
+Motion::Project::App.setup do |app|
   lib_dir_path = File.dirname(File.expand_path(__FILE__))
   app.files.unshift File.join(lib_dir_path, "project/product.rb")
   app.files.unshift File.join(lib_dir_path, "project/iap.rb")
